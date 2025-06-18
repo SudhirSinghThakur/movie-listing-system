f8744296b58ce5a88b2f0aa6c2dbbdf64b6cd8b1
Sudhir Singh Thakur <sudhirthakur4@gmail.com>
2025-06-19 01:24:07 +0530

    small changes

--- backend-api/MultipeAuthenticationSupport/Program.cs
+++ backend-api/MultipeAuthenticationSupport/Program.cs
@@ -193,7 +193,8 @@ public class Program
 
         try
         {
-            app.Run();
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            app.Run($"http://0.0.0.0:{port}");
         }
         catch (Exception ex)
         {
